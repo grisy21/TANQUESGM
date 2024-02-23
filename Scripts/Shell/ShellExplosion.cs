6a1c337fda116a6c0044c740a4bbaad6cd4a702e
grisy21 <grisi8400@gmail.com>
2024-02-22 23:00:39 -0400

    agg

---
+++ Scripts/Shell/ShellExplosion.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask m_TankMask;
+    public ParticleSystem m_ExplosionParticles;       
+    public AudioSource m_ExplosionAudio;              
+    public float m_MaxDamage = 100f;                  
+    public float m_ExplosionForce = 1000f;            
+    public float m_MaxLifeTime = 2f;                  
+    public float m_ExplosionRadius = 5f;              
+
+
+    private void Start()
+    {
+        //Si no se ha destruido aún, destruir la bomba después de su tiempo de vida
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Recoge los colliders en una esfera desde la posición de la bomba con el radio máximo.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+
+        //Recorro los colliders
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            //Selecciono su Rigidbody.
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+            //Si no tienen, paso al siguiente
+            if (!targetRigidbody)
+                continue;
+
+            //Añado la fuerza de la explosion
+            targetRigidbody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+
+            //Busco el script TankHealth asociado con el Rigidbody
+            TankHealth tankHealth = targetRigidbody.GetComponent<TankHealth>();
+
+            //Si no hay script TankHealt, paso al siguiente
+            if (!targetRigidbody)
+                continue;
+
+            //Calculo el daño a aplicar en funcion de la distancia a la bomba
+
+            float damage = CalculateDamage(targetRigidbody.position);
+
+            //Aplico el daño al tanque
+            tankHealth.TakeDamage(damage);
+        }
+
+        //Desanclo el sistema de particulas de la bomba
+        m_ExplosionParticles.transform.parent = null;
+
+        //Repropduzco el sistema de particulas
+        m_ExplosionParticles.Play();
+
+        //Reproduzco el audio
+        m_ExplosionAudio.Play();
+
+        //Cuando las particulas han terminado, destruyo su objeto asociado
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+
+        //Destruyo la bomba
+        Destroy(gameObject);
+    }
+
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        //Creo un vector desde la bomba al objetivo
+        Vector3 explosionToTarget = targetPosition - transform.position;
+
+        //Calculo la distancia desde la bomba al objetivo
+        float explosionDistance = explosionToTarget.magnitude;
+
+        //Calculo la proporcion de maxima distancia (radio maximo) desde la explosion al tanque
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
+
+        //Calculo el daño a esa proporcion
+        float damage = relativeDistance * m_MaxDamage;
+
+        //Me aseguro de que el minimo daño es 0.
+        damage = Mathf.Max(0f, damage);
+
+        //Devuelvo el daño
+        return damage;
+    }
+}
---
+++ Scripts/Tank/TankMovement.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    public int m_PlayerNumber = 1; //Se utiliza para identificar que tanque es de cada jugador.
+    public float m_Speed = 12f; // Rapidez con la que 1 tanque se mueve alante y atras.
+    public float m_TurnSpeed = 180f;       //Rapidez de giro del tanque en grados por segundo.
+    public AudioSource m_MovementAudio;    //Referencia de audio del motor del tanque (diferente del de disparo).
+    public AudioClip m_EngineIdling;       //Audio a reproducir cuando el tanque no se mueve.
+    public AudioClip m_EngineDriving;      //Audio a reproducir cuando el tanque se mueve.
+    public float m_PitchRange = 0.2f;     //Cantidad de variacion de afinacion del audio del motor (para que sea mas real).
+
+
+
+    private string m_MovementAxisName;      // Nombre del eje para moverse alante y atrás.
+    private string m_TurnAxisName;         //Nombre del eje para girar.
+    private Rigidbody m_Rigidbody;         //Referencia del componente para mover el tanque.
+    private float m_MovementInputValue;    //Valor actual de entrada para el movimiento.
+    private float m_TurnInputValue;        //Valor actual de entrada para el giro.
+    private float m_OriginalPitch;         //Valor del pitch de la fuente de audio al inicio.
+
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    private void OnEnable()
+    {
+        // Al arrancar/habilitar el tanque, deshabilitamos la kinemática del tanque para que se pueda mover.
+        m_Rigidbody.isKinematic = false;
+
+        // Reseteamos los valores de entrada.
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+    }
+
+
+    private void OnDisable()
+    {
+        // Al parar/deshabilitar el tanque, habilitamos la kinemática del tanque para que se pare. 
+        m_Rigidbody.isKinematic = true;
+    }
+
+
+    private void Start()
+    {
+        //Nombres de los ejes segun el numero de jugador.
+        m_MovementAxisName = "Vertical" + m_PlayerNumber;
+        m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+
+        //Almaceno la afinacion original del audio del motor.
+        m_OriginalPitch = m_MovementAudio.pitch;
+    }
+
+    private void Update()
+    {
+        // Almaceno los valores de entrada. 
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+
+        //Llamo a la función que gestiona el audio del motor
+        EngineAudio();
+    }
+
+
+    private void EngineAudio()
+    {
+        //Si no hay entrada, es que esta quieto...
+        if (Mathf.Abs(m_MovementInputValue) < 0.1f && Mathf.Abs(m_TurnInputValue) < 0.1f)
+        {
+            // ... y si estaba reproduciendo el audio de moverse...
+            if (m_MovementAudio.clip == m_EngineDriving)
+            {
+                // ... cambio el aduio al de estar parado y lo reproduzco.
+
+                m_MovementAudio.clip = m_EngineIdling;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+        }
+        else
+        {
+
+            // Si hay entrada es que se esta moviendo. Si estaba reproduciendo el esta parado...
+            if (m_MovementAudio.clip == m_EngineIdling)
+            {
+                //... cambio el aduio al de moverse y lo reproduzco.
+                m_MovementAudio.clip = m_EngineDriving;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+        }
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Ajusto la posicion y rotacion
+        Move();
+        Turn();
+    }
+
+
+    private void Move()
+    {
+        // Creo un vector en la dirección en la que apunta el tanque,
+        // con una magnitud basada en la entrada, la velocidad y el tiempo entre frames. 
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+        // Aplico ese vector de movimiento al rigidbody del tanque
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+    }
+
+    private void Turn()
+    {
+        // Calculo el número de grados de rotación basandome en la entrada, la velocidad y el tiempo entre frames.
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        // Convirto ese numero en una rotacion en el eje Y.
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        // Aplico esa rotacion al rigidbody del tanque.
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+    }
+}
